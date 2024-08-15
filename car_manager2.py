b7c1052dc16f251a5e4de6deff9f4aeff5fbcced
SzymonBryniak <szymonbryniakproject@gmail.com>
2024-08-15 21:41:04 +0100

    I want to use deltaTime to space out car spawns like in the game shown on Udemy. The delta module contains an example of delta time script I want to base my code on.

--- car_manager2.py
+++ car_manager2.py
@@ -11,32 +11,25 @@ class CarManager:
 
     def __init__(self):
         self.cars = []
-
-        self.create_cars()
+        self.create_car()
         self.cars_at_end = 0
         self.car_count = 0
-
-    def create_cars(self):
-        car = Turtle()
-        car.penup()
-        car.shape('square')
-        car.color(random.choice(COLORS))
-        car.shapesize(stretch_len=2)
-        car.setpos(x=320, y=random.choice(STARTING_POSITIONS))
-        car.setheading(180)
-        self.cars.append(car)
-        # for color in COLORS:
-        #     car = Turtle()
-        #     car.penup()
-        #     car.shape('square')
-        #     car.color(color)
-        #     car.shapesize(stretch_len=2)
-        #     car.setpos(x=320, y=random.choice(STARTING_POSITIONS))
-        #     car.setheading(180)
-        #     self.cars.append(car)
+        self.timer = 0
 
     def create_car(self):
-        timer = 0
+        for i in range(5):
+            car = Turtle()
+            car.penup()
+            car.shape('square')
+            car.color(random.choice(COLORS))
+            car.shapesize(stretch_len=2)
+            car.setpos(x=320, y=random.choice(STARTING_POSITIONS))
+            self.cars.append(car)
+
+    def create_cars(self):
+        time.sleep(2)
+        self.timer += 1
+        max_timer = 40
         if self.car_count < 60:
             # while timer != 1:
             self.car_count += 1
@@ -50,9 +43,28 @@ class CarManager:
             car.shapesize(stretch_len=2)
             car.setpos(x=320, y=STARTING_POSITIONS[random_position])
             self.cars.append(car)
-            timer += 0.01
+            max_timer += 40
 
-
+    def create_cars_2(self):
+        self.timer += 1
+        max_timer = 100
+        if self.timer < max_timer:
+            return
+        elif self.car_count < 60:
+            # while timer != 1:
+            self.car_count += 1
+            print(f'cars total {self.car_count}')
+            random_color = random.randint(0, len(COLORS)-1)
+            random_position = random.randint(0, len(STARTING_POSITIONS)-1)
+            car = Turtle()
+            car.penup()
+            car.shape('square')
+            car.color(COLORS[random_color])
+            car.shapesize(stretch_len=2)
+            car.setpos(x=320, y=STARTING_POSITIONS[random_position])
+            self.cars.append(car)
+            max_timer += 40
+            return
     # def check_space(self):
     #     for car in self.cars:
     #         if car
@@ -63,25 +75,24 @@ class CarManager:
 
     def move_cars(self):
         end_range = len(self.cars)
-        # print(end_range)
 
         for car in range(0, self.car_count):
-            print(self.cars[car].xcor())
+            # print(self.cars[car].xcor())
             if self.cars[car].xcor() > -280:
+
                 random_move = random.randint(2, 15)
                 self.cars[car].goto(y=self.cars[car].ycor(), x=self.cars[car].xcor() - random_move * 0.3)
-
             else:
-                print(f'car reached xcor : {self.cars[car].xcor()}')
-
+                # print(f'car reached xcor : {self.cars[car].xcor()}')
                 self.cars_at_end += 1
-
                 # self.cars[car].setpos(x=280, y=self.cars[car].ycor())  # to select a new random y cor
                 random_position = random.choice(STARTING_POSITIONS)
                 self.cars[car].setpos(x=280, y=random_position)
-        # print(self.cars[1].xcor())
-        self.create_car()
+
+        # self.create_cars_2()
+        self.create_cars_2()
         return
+
 
 
 
